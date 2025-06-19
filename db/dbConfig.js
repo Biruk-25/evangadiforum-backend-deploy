35242ad8e54e6738359712878f15e79907266453
Biruk Umeko <birukelias2@gmail.com>
2025-06-19 10:31:54 -0500

    final commit db

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,13 +1,27 @@
 
-
 const mysql2 = require('mysql2');
 
 const dbConnection = mysql2.createPool({
-  user: process.env.USER,
+  host: process.env.DB_HOST,           
+  user: process.env.DB_USER,           
   password: process.env.PASSWORD,
-  host: 'localhost',
-  database: process.env.DATABASE,
+  database: process.env.DB_NAME,
   connectionLimit: 10
 });
 
-module.exports=dbConnection.promise()
+module.exports = dbConnection.promise();
+
+
+
+
+// const mysql2 = require('mysql2');
+
+// const dbConnection = mysql2.createPool({
+//   user: process.env.USER,
+//   password: process.env.PASSWORD,
+//   host: 'localhost',
+//   database: process.env.DATABASE,
+//   connectionLimit: 10
+// });
+
+// module.exports=dbConnection.promise()
